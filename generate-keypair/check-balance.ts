3060ed06a52143ccae6a2cc6ee9e90f53f71bb30
daktic <gberg@dakticdigital.com>
2025-01-02 11:34:54 -0500

    Can now read sns domain names

--- generate-keypair/check-balance.ts
+++ generate-keypair/check-balance.ts
@@ -1,4 +1,7 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+import {
+    getDomainKeySync
+} from "@bonfida/spl-name-service";
 
 const publicKey = new PublicKey("3mHXRQa5AUGt4PYtJ93yT4A7axqtTeEPJekuKfnkufuh");
 const connection = new Connection("https://api.devnet.solana.com","confirmed");
@@ -9,3 +12,26 @@ const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL;
 console.log(
     `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSol}!`,
 )
+
+// new connection to mainnet
+
+const mainConnection = new Connection("https://api.mainnet-beta.solana.com","confirmed");
+
+const domains_to_check = [
+    "toly.sol",
+    "shaq.sol",
+    "mccann.sol",
+    ];
+
+for (const domain of domains_to_check){
+    // convert SNS to public key
+    const { pubkey } = getDomainKeySync(domain);
+
+    const balanceInLamports = await connection.getBalance(pubkey);
+
+    const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL;
+
+    console.log(
+        `💰 Finished! The balance for the wallet at address ${domain} is ${balanceInSol}!`,
+    )
+}
